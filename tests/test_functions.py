6704a8d93610513dbf08890bcc1482b2ac7ebf04
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-22 13:29:53 +0400

    Добавил функцию перевода даты из str в datetime и тест к ней.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,8 +1,15 @@
 import pytest
 from coursework3.functions import *
+from datetime import datetime
 
 
 def test_load_json():
     """ Проверка соответствия типа загружаемых данных. """
     assert type(load_json('operations_copy.json')) == list
     assert type(load_json('operations_copy.json')[0]) == dict
+
+
+def test_date_format():
+    new_date = datetime(2024, 2, 22, 10, 30, 20, 294041)
+    """ Проверка перевода даты из str в datetime. """
+    assert date_format("2024-02-22T10:30:20.294041") == new_date

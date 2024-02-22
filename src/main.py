c6fd53c8cd41a45b59f8f08b43875df61b0b28fb
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-22 15:49:21 +0400

    Добавил итоговый функционал.

--- src/main.py
+++ src/main.py
@@ -16,3 +16,27 @@ def get_sort_transactions(json_path) -> list:
                            reverse=True)
 
     return list_transactions
+
+
+def show_transactions():
+    """ Выводит 5 крайних успешных транзакций. """
+    five_transaction = get_sort_transactions('operations.json')
+    count_executed = 0
+    count = 0
+    while count_executed < 5:
+        if five_transaction[count].is_executed():
+            print(f'{five_transaction[count].get_date_str()} '
+                  f'{five_transaction[count].get_description()}')
+            if five_transaction[count].get_from_account() is None:
+                print(f'{five_transaction[count].get_to_account()}')
+            else:
+                print(f'{five_transaction[count].get_from_account()} -> '
+                      f'{five_transaction[count].get_to_account()}')
+            print(f'{five_transaction[count].get_amount()} '
+                  f'{five_transaction[count].get_currency()}\n')
+            count_executed += 1
+        count += 1
+
+
+if __name__ == '__main__':
+    show_transactions()

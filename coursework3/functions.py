6226c7633d49589acce9497083915e146644e9af
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-22 13:16:09 +0400

    Реализация функции load_json

--- coursework3/functions.py
+++ coursework3/functions.py
@@ -4,4 +4,7 @@ from datetime import datetime
 
 def load_json(json_file) -> list:
     """ Считывает json данные по транзакциям и возвращает список словарей. """
-    pass
+    with open(json_file, 'r', encoding='utf-8') as f:
+        file = f.read()
+        file = json.loads(file)
+    return file
